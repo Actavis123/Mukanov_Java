602418ccce2be822a82ab67487d0a5405fbd83ba
Actavis123 <alisherindeed@gmail.com>
2026-08-29 20:49:42 +0500

    Added Task 1.2

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        //Task 1.1
+        // Task 1.1
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
+        scanner.nextLine();
+
         if (number > 7) {
             System.out.println("Hello");
         }
+
+        // Task 1.2
+        System.out.print("Enter a name: ");
+        String name = scanner.nextLine();
+
+        if (name.equals("John")) {
+            System.out.println("Hello, John");
+        } else {
+            System.out.println("There is no such name");
+        }
+
         scanner.close();
     }
 }
