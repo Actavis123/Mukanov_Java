3ddf12f68b5468fb80283e713cdae41d2a5e8aa2
Actavis123 <alisherindeed@gmail.com>
2026-08-29 20:20:29 +0500

    Added Task 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        //Task 1.1
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number > 7) {
+            System.out.println("Hello");
+        }
+        scanner.close();
+    }
 }
